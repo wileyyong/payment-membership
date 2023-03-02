305c07970452258cf022e6ead497ff2a2fd78e3b
Dev <dev@Devs-MacBook-Pro.local>
2023-03-02 00:40:40 -0600

    updated constants

--- src/components/PaymentTab/StripeContainer.js
+++ src/components/PaymentTab/StripeContainer.js
@@ -2,8 +2,9 @@ import React from 'react'
 import { Elements } from '@stripe/react-stripe-js'
 import { loadStripe } from '@stripe/stripe-js'
 import PaymentForm from './PaymentForm'
+import constants from '../../constant'
 
-const PUBLIC_KEY = "pk_test_51MbfzNLlfbjWJ6bQHhWR4gLBA381DRjR7LtI4k0YL2KotRFgjnLuFJsZ7DnXyEDJysGPeECE6EGzodNL7WcgTsz400t5IsVHNz"
+const PUBLIC_KEY = constants.PUBLIC_KEY;
 
 const stripeTestPromise = loadStripe(PUBLIC_KEY)
 
--- src/constant.js
+++ src/constant.js
@@ -1,8 +1,9 @@
 
 
 const constants= {
-    baseURL:"https://membership-backend-mu.vercel.app"
+    baseURL:"https://membership-backend-mu.vercel.app",
     // baseURL:"http://localhost:7008"
+    PUBLIC_KEY: "pk_test_51MbfzNLlfbjWJ6bQHhWR4gLBA381DRjR7LtI4k0YL2KotRFgjnLuFJsZ7DnXyEDJysGPeECE6EGzodNL7WcgTsz400t5IsVHNz"
 }
 
 export default constants;
